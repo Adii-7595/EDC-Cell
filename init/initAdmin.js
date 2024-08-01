2ae7e00842319f584412e2ad7b75fb534e96c911
Aditya Gupta <rajaditya2399@gmail.com>
2024-08-02 01:14:00 +0530

    pre-final

--- init/initAdmin.js
+++ init/initAdmin.js
@@ -1,11 +1,16 @@
-
 const mongoose = require('mongoose');
 const Admin = require('../models/admin');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const dbUrl = process.env.DATABASE_URL;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/edc', {
-    
-});
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Function to create the initial admin user
 async function createAdmin() {
@@ -13,14 +18,13 @@ async function createAdmin() {
     const password = 'Edc@123'; // Change to your desired password
 
     try {
-        
         const admin = new Admin({ username, password });
 
         await admin.save();
         console.log('Admin user created successfully');
-        mongoose.connection.close();
     } catch (err) {
         console.error('Error creating admin user:', err);
+    } finally {
         mongoose.connection.close();
     }
 }
